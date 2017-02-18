933ada85f3fa27c391a336e52e1afad2cf02656b
STakanashi <s.takanashi0120@gmail.com>
2017-02-19 01:23:22 +0900

    Update examples

---
+++ examples/section_force_calculation/section_force_calculation.py
@@ -0,0 +1,25 @@
+from pyPanair.postprocess import calc_section_force
+
+
+def inputloop(message, outtype):
+    while True:
+        try:
+            print(message, end="")
+            inp = outtype(input())
+            return inp
+        except ValueError:
+            pass
+
+
+if __name__ == '__main__':
+    # test calculation of section forces
+    aoa = inputloop("enter the AoA of the case: ", float)
+    mac = inputloop("enter the mac of the wing: ", float)
+    print("enter the center of rotation")
+    rot_center = ()
+    for cor in ("x", "y", "z"):
+        rot_center += (inputloop("{}: ".format(cor), float), )
+    casenum = inputloop("enter the case number: ", int)
+    networknum = inputloop("enter the network number of the wing: ", int)
+
+    calc_section_force(aoa, mac, rot_center, casenum, networknum)
--- pyPanair/postprocess/__init__.py
+++ pyPanair/postprocess/__init__.py
@@ -1,2 +1,3 @@
 from .agps_converter import *
 from .ffmf_converter import *
+from .section_force import *
