ad6d980b10dfb64204be9dc66838b2dbfab31cac
Unknown <s.takanashi0120@gmail.com>
2017-02-26 17:31:58 +0900

    Add exapmle for batch analysis

--- examples/batch_analysis/batch_analysis.py
+++ examples/batch_analysis/batch_analysis.py
@@ -1,85 +1,90 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from glob import glob
 from logging import getLogger, StreamHandler, FileHandler, Formatter, INFO
-from multiprocessing import cpu_count
-from multiprocessing.pool import ThreadPool
-from os import remove, makedirs
-from os.path import isdir, join
+import multiprocessing
+import os
 from shutil import copy2, rmtree
 from subprocess import Popen, PIPE
 from numpy import genfromtxt
-import makewgs_aux_model3
+from make_wgs_aux import main as mkwgsaux
+from pathlib import Path
+from concurrent import futures
 
 
-def panin(folder, aux):
-    process = Popen("./panin", stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=folder)
+def run_panin(directory, aux):
+    process = Popen("./panin", stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=directory)
     aux = str.encode(aux)
     stdout, stderr = process.communicate(aux)
     if stderr:
-        logger.error("\n".join((folder, stderr)))
+        logger.error("\n".join((directory, stderr)))
     else:
-        logger.debug("\n".join((folder, stdout)))
+        logger.debug("\n".join((directory, stdout)))
 
 
-def panair(folder):
-    process = Popen("./panair", stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=folder)
+def run_panair(directory):
+    process = Popen("./panair", stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=directory)
     stdout, stderr = process.communicate(b"a502.in")
-    if "PanAir is stopping." in open(join(folder, "panair.err")).read():
-        logger.critical("\n".join((folder, "fatal error with PanAir")))
+    if "PanAir is stopping." in open(os.path.join(directory, "panair.err")).read():
+        logger.critical("\n".join((directory, "fatal error with PanAir")))
     if stderr:
-        logger.error("\n".join((folder, stderr)))
+        logger.error("\n".join((directory, stderr)))
     else:
-        logger.debug("\n".join((folder, stdout)))
+        logger.debug("\n".join((directory, stdout)))
 
 
 def safe_makedirs(path):
     try:
-        makedirs(path)
+        os.makedirs(path)
     except OSError:
-        if not isdir(path):
+        if not os.path.isdir(path):
             logger.error("could not create directory at {}".format(path))
 
 
-def copy2folder(files, oldfolder, newfolder):
-    safe_makedirs(newfolder)
+def copy2dir(files, olddir, newdir):
+    safe_makedirs(newdir)
     if type(files) is str:
-        files = (files)
+        files = (files, )
     for f in files:
         try:
-            copy2("{0}/{1}".format(oldfolder, f), "{0}/{1}".format(newfolder, f))
+            copy2("{0}/{1}".format(olddir, f), "{0}/{1}".format(newdir, f))
         except FileNotFoundError as e:
             logger.error(e)
 
-def clean(root, casename):
-    # 前回使用した中間ファイルを削除
-    folders = [f for f in glob(join(root, "panair[0-9]")) if isdir(f)]
-    for f in folders:
-        try:
-            rmtree(f)
-        except WindowsError:
-            logger.warning("failed to delete {}".format(f))
 
-    # auxを削除
-    subcases = glob(join(root, "{}_[0-9].aux".format(casename)))
-    for f in subcases:
-        try:
-            remove(f)
-        except WindowsError:
-            logger.warning("failed to delete {}".format(f))
+def run_analysis(casenum, params):
+    # create directory to run panin and panair
+    procid = int(multiprocessing.current_process().pid)
+    target_dir = "Z:/panair{}".format(procid)
+    safe_makedirs(target_dir)
+
+    # run panin and panair
+    mkwgsaux(target_dir=target_dir, **params)
+    run_panin(target_dir, auxname)
+    run_panair(target_dir)
+
+    # save results
+    files_to_save = ("panair.out", "ffmf", "agps", "panair.err", "panin.dbg", "a502.in")
+    newdir = os.path.join("results", "case{}".format(casenum))
+    copy2dir(files_to_save, target_dir, newdir)
+
+    # delete intermediate files
+    rmtree(target_dir)
+    
+    print("test!")
 
 
 if __name__ == '__main__':
-    # 初期設定
-    casename = "model3"
-    wgsname = casename + ".wgs"
-    enable_ramdisk = False # ramdiskを使用する場合はTrue
+    # initialize
+    N_PROCS = 2
+    wgsname = "ADODG3.wgs"
+    auxname = "ADODG3.aux"
+    enable_ramdisk = True # if True, intermediate files will be placed at `root`
     if enable_ramdisk:
-        root = "Z:/" # ramdiskのpath
+        root = "Z:/" # path of the directory for intermediate files
     else:
         root = "./"
 
-    # ロガーの設定
+    # initialize logger
     logger = getLogger(__name__)
     shandler = StreamHandler()
     shandler.setFormatter(Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s"))
@@ -90,50 +95,18 @@ if __name__ == '__main__':
     logger.setLevel(INFO)
     logger.addHandler(shandler)
     logger.addHandler(fhandler)
-    logger.info("started program")
+    logger.info("start batch analysis")
 
     # caselistを読み込む
     caselist = genfromtxt("caselist.csv", delimiter=',', names=True)
-    for case in caselist:
-        dvs = {"xfin":case["x"], "yfin":case["y"], "delta":case["delta"],
-               "span_fin":case["b"], "rchord_fin":case["cr"],
-               "taper":case["taper"], "sweep":case["sweep"]}
-        try:
-            dvs["zfin"] = case["z"]
-        except ValueError:
-            logger.debug("zfin is not specified")
-        if case["flag"] == 1:
-            logger.debug("skipping case{}".format(int(case["casenum"])))
-            continue
-        print(dvs)
-
-        clean(root, casename)
-
-        logger.info("calculating case{}".format(int(case["casenum"])))
-        makewgs_aux_model3.main(**dvs)
-        subcases = glob("{}_[0-9].aux".format(casename))
-        subcase_folders = [join(root, "panair{}".format(i+1)) for (i, _) in enumerate(subcases)]
-        # paninを起動
-        num = min(len(subcases),cpu_count())  # 1度に使用するスレッド数（Noneなら上限まで使用）
-        tp = ThreadPool(num)
-        for (subcase, folder) in zip(subcases, subcase_folders):
-            copy2folder((subcase, wgsname), ".", folder)
-            tp.apply_async(panin, (folder, subcase, ))
-        tp.close()
-        tp.join()
-
-        # panairを起動
-        tp = ThreadPool(num)
-        for folder in subcase_folders:
-            tp.apply_async(panair, (folder,))
-        tp.close()
-        tp.join()
-
-        # 結果を保存
-        files_to_save = ("panair.out", "ffmf", "agps", "panair.err", "panin.dbg", wgsname)
-        for folder in subcase_folders:
-            newfolder = join("results", "case{0}".format(int(case["casenum"])), folder.lstrip(root))
-            copy2folder(files_to_save, folder, newfolder)
-
-
-    logger.info("finished program")
+    with futures.ProcessPoolExecutor(max_workers=N_PROCS) as executor:
+        for case in caselist:
+            casenum = int(case["casenum"])
+            params = {s: case[s] for s in ("x1", "y1", "x2", "y2", "y3")}
+            if case["run"] == 1:
+                logger.info("calculating case{}".format(int(case["casenum"])))
+                executor.submit(run_analysis, casenum, params)
+            else:
+                logger.debug("skipping case{}".format(casenum))
+                continue
+    logger.info("finish batch analysis")
--- examples/batch_analysis/make_wgs_aux.py
+++ examples/batch_analysis/make_wgs_aux.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python
-from functools import partial
+import os
 import numpy as np
 from scipy.interpolate import interp1d
 from pyPanair.preprocess import wgs_creator
 from pyPanair.utilities import bspline
 
 
-def main(target_dir=None, twist_func=None, aoas=(7.42)):
+def main(x1, x2, y1, y2, y3, aoas=(7.42), target_dir=""):
     """ create a LaWGS file for twisted rectangular wing
     reference case 3"""
     wgs = wgs_creator.LaWGS("model for ADODG case3")
-    n_wing_x = 40
-    n_wing_y = 40
-    n_wing_z = 12
+    n_wing_x = 30
+    n_wing_y = 30
+    n_wing_z = 8
     chord_wing = 1.
     halfspan_wing = chord_wing * 3
 
-    cv = np.array([[0., 0.],
-                   [0., -4.999691458172163649],
-                   [0.5000349654015733281, -2.231521636034730971],
-                   [1., -4.732433281976027750]])
-
-    # n_cpoints = 4
-    # sep = 1 / (n_cpoints - 2)
-    # cv = np.zeros((n_cpoints, 2))
-    # for i in range(1, n_cpoints-1):
-    #     cv[i,0] = np.random.random() * sep + sep * (i-1)
-    # cv[-1,0] = 1
-    # cv[1:,1] = np.random.random(cv.shape[0]-1) * 20 -10
+    # define twist distribution
+    cv = np.array(((0, 0),
+                   (x1, y1),
+                   (x2, y2),
+                   (1, y3)))
     twist_func = bspline(cv, degree=3, periodic=False)
-
     twist_xy = twist_func(np.linspace(0, 1, 150))
     twist_dist = interp1d(twist_xy[:,0] * halfspan_wing, twist_xy[:,1])
 
@@ -40,12 +32,11 @@ def main(target_dir=None, twist_func=None, aoas=(7.42)):
         rot_foil = base_airfoil.roty(base_airfoil[0], twist_dist(y))
         rot_foil.shift((0, y, 0), inplace=True)
         wing.append(rot_foil)
-    wingtipu, _ = wing[-1].split_half()
     wing = wgs_creator.Network(wing)
     wgs.append_network("wing", wing, 1)
 
-    # wingtip = wingtipu.linspace(wingtipl.flip(), n_wing_z) # flat tip
     degs = np.linspace(0, -180, n_wing_z)
+    wingtipu = base_airfoil.shift((0, halfspan_wing, 0))
     wingtip = [wingtipu.rotx(wingtipu[0], d) for d in degs]
     wingtip = wgs_creator.Network(wingtip)
     wingtip = wingtip.roty(wingtipu[0], twist_dist(halfspan_wing))
@@ -56,15 +47,16 @@ def main(target_dir=None, twist_func=None, aoas=(7.42)):
     wingwake = wing.make_wake(3, wake_length)
     wgs.append_network("wingwake", wingwake, 18)
 
-    wgs.create_wgs("ADODG3.wgs".format(target_dir))
+    wgs.create_wgs(os.path.join(target_dir, "ADODG3.wgs"))
 
     span = halfspan_wing * 2
     sref = chord_wing * span
     xref = chord_wing * 0.25
-    wgs.create_aux("ADODG3.aux".format(target_dir), alpha=aoas, mach=0.5, cbar=chord_wing, span=span, sref=sref,
+    aux_name = os.path.join(target_dir, "ADODG3.aux")
+    wgs.create_aux(aux_name, alpha=aoas, mach=0.5, cbar=chord_wing, span=span, sref=sref,
                    xref=xref, zref=0.)
 
-    wgs.create_stl("ADODG3.stl")
+    # wgs.create_stl("ADODG3.stl")
 
 
 if __name__ == '__main__':
