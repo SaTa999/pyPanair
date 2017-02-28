f4cd2fa4b41ac877f1167fb4c264d38cc3088b0d
Unknown <s.takanashi0120@gmail.com>
2017-02-28 17:32:11 +0900

    Fix utilities

--- pyPanair/utilities.py
+++ pyPanair/utilities.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 from scipy.interpolate import splev
 
@@ -14,18 +15,19 @@ def bspline(cv, degree=3, periodic=False):
     cv = np.array(cv)
     count = cv.shape[0]
     if periodic:
-        factor, fraction = divmod(count+degree+1, count)
+        factor, fraction = divmod(count + degree + 1, count)
         cv = np.concatenate((cv,) * factor + (cv[:fraction],))
         count = len(cv)
-        degree = np.clip(degree,1,degree)
+        degree = np.clip(degree, 1, degree)
     # If opened, prevent degree from exceeding count-1
     else:
-        degree = np.clip(degree,1,count-1)
+        degree = np.clip(degree, 1, count - 1)
     # Calculate knot vector
     if periodic:
-        kv = np.arange(0-degree,count+degree+degree-1,dtype='int')
+        kv = np.arange(0 - degree, count + degree + degree - 1, dtype='int')
     else:
-        kv = np.array([0]*degree + list(range(count-degree+1)) + [count-degree]*degree,dtype='int')
+        kv = np.array([0] * degree + list(range(count - degree + 1)) + [count - degree] * degree, dtype='int')
+
     def spl(u, normalize=True):
         """normalize： normalize the parameter u so, u=1 at the end of the curve"""
         # Calculate result
@@ -37,28 +39,30 @@ def bspline(cv, degree=3, periodic=False):
                 u *= count - degree
         points = np.zeros((len(u), cv.shape[1]))
         for i in range(cv.shape[1]):
-            points[:,i] = splev(u, (kv,cv[:,i],degree))
+            points[:, i] = splev(u, (kv, cv[:, i], degree))
         return points
+
     return spl
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+
     n_cpoints = 4
     sep = 1 / (n_cpoints - 2)
     cv = np.zeros((n_cpoints, 2))
-    for i in range(1, n_cpoints-1):
-        cv[i,0] = np.random.random() * sep + sep * (i-1)
-    cv[-1,0] = 1
-    cv[1:,1] = np.random.random(cv.shape[0]-1) * 2 -1
-
+    for i in range(1, n_cpoints - 1):
+        cv[i, 0] = np.random.random() * sep + sep * (i - 1)
+    cv[-1, 0] = 1
+    cv[1:, 1] = np.random.random(cv.shape[0] - 1) * 2 - 1
 
     plt.plot(cv[:, 0], cv[:, 1], 'o', label='Control Points')
 
     us = np.linspace(0, 1, 100)
     for d in range(1, 4):
-        res =  bspline(cv, degree=d, periodic=False)(us)
-        x = res[:,0]
-        y = res[:,1]
+        res = bspline(cv, degree=d, periodic=False)(us)
+        x = res[:, 0]
+        y = res[:, 1]
         plt.plot(x, y, '-', label='Degree %s' % d)
 
     plt.minorticks_on()
