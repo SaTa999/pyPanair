907ac34c2f52bc5feb1d94ec02137aa1bc0bd00c
Unknown <s.takanashi0120@gmail.com>
2017-02-28 17:28:03 +0900

    Fix agps_converter

--- pyPanair/postprocess/agps_converter.py
+++ pyPanair/postprocess/agps_converter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-__author__ = "stakanashi"
 import numpy as np
 import os
 
@@ -20,18 +19,18 @@ def read_column(file, firstline):
     return column
 
 
-def read_network(file, firstheader):
+def read_network(file, header):
     """read a network"""
-    network_n = int(firstheader[0][1:3]) # get network number from the first header (e.g. 01 from n01c001)
+    network_n = int(header[0][1:3])  # get network number from the first header (e.g. 01 from n01c001)
     # print("loading network no.", network_n)
     network = list()
-    line = firstheader
+    line = header
     # register methods for faster evaluation
     network_append = network.append
     # read each line until next header
     while line:
         col = read_column(file, line)
-        network.append(col)
+        network_append(col)
         line = file.readline().split()
         # break at the end of agps file
         if not line:
@@ -48,9 +47,9 @@ def read_agps(inputfile="agps"):
     with open(inputfile, "r") as f:
         # skip the header of the agps file
         for _ in range(6):
-            line = f.readline()
+            f.readline()
         line = f.readline().split()
-        f.readline() # skip the header of first network ("icol, x, y, z, cp1, cp2, cp3, cp4")
+        f.readline()  # skip the header of first network ("icol, x, y, z, cp1, cp2, cp3, cp4")
         dat = []
         while line:
             net, line = read_network(f, line)
@@ -62,36 +61,36 @@ def write_vtk(n_wake=0, outputname="agps", inputfile="agps"):
     """Write agps in the legacy paraview format (vtk)
     All networks will be merged into one block
     Therefore, user are advised to omit 'wakes' by specifying the 'n_wakes'"""
-    data = read_agps(inputfile) # read agps file & specify the number of networks to omit
+    data = read_agps(inputfile)  # read agps file & specify the number of networks to omit
     print("n_wake = ", n_wake)
     # write the header of the vtk file
     vtk = "# vtk DataFile Version 2.0\n"
     vtk += "scalar\n"
     vtk += "ASCII\n"
     vtk += "DATASET UNSTRUCTURED_GRID\n"
-    n_points = 0 # number of points in vtk file
-    n_cells = 0 # number of quadrilateral cells formed by the points
+    n_points = 0  # number of points in vtk file
+    n_cells = 0  # number of quadrilateral cells formed by the points
     n_cp = data[0].shape[2] - 4
-    points = str() # coordinate of each point (x, y, z)
-    point_data = [str()] * n_cp # cp at each point (cp1, cp2, cp3, cp4)
-    cells = str() # ids of each quadrilateral cell (e.g. (0, n_col, n_col + 1, 1) for first cell)
-    for i in range(len(data)-n_wake):
+    points = str()  # coordinate of each point (x, y, z)
+    point_data = [str()] * n_cp  # cp at each point (cp1, cp2, cp3, cp4)
+    cells = str()  # ids of each quadrilateral cell (e.g. (0, n_col, n_col + 1, 1) for first cell)
+    for i in range(len(data) - n_wake):
         net = data[i]
         n_row = int(net.shape[0])
         n_col = int(net.shape[1])
-        print("network {} shape: ".format(i+1), net.shape)
-        base_square = np.array((0, n_col, n_col+1, 1))
+        print("network {} shape: ".format(i + 1), net.shape)
+        base_square = np.array((0, n_col, n_col + 1, 1))
         for j in range(n_row):
             for k in range(n_col):
-                point = net[j,k]
+                point = net[j, k]
                 # add coordinate of a point
                 points += "{0} {1} {2}\n".format(point[1], point[2], point[3])
                 # add cp data of a point
                 for l in range(n_cp):
-                    point_data[l] += "{}\n".format(point[4+l])
+                    point_data[l] += "{}\n".format(point[4 + l])
                 # add ids of a cell
-                if not j == n_row-1 and not k == n_col-1:
-                    square =  base_square + (j * n_col + k) + n_points
+                if not j == n_row - 1 and not k == n_col - 1:
+                    square = base_square + (j * n_col + k) + n_points
                     square = (str(p) for p in square)
                     cells += "4 " + " ".join(square) + "\n"
         # add the number of points / cells
@@ -99,11 +98,11 @@ def write_vtk(n_wake=0, outputname="agps", inputfile="agps"):
         n_cells += (n_row - 1) * (n_col - 1)
     # write the header of each block (POINTS, CELLS, CELLTYPES, POINT_DATA)
     points = "POINTS {} float\n".format(n_points) + points
-    cells = "CELLS {0} {1}\n".format(n_cells, n_cells*5) + cells
-    cell_types = "CELL_TYPES {}\n".format(n_cells) + "9\n"*n_cells
+    cells = "CELLS {0} {1}\n".format(n_cells, n_cells * 5) + cells
+    cell_types = "CELL_TYPES {}\n".format(n_cells) + "9\n" * n_cells
     vtk += points + cells + cell_types + "POINT_DATA {}\n".format(n_points)
     for l in range(n_cp):
-        vtk += "SCALARS cp{} float\nLOOKUP_TABLE default\n".format(l+1) + point_data[l]
+        vtk += "SCALARS cp{} float\nLOOKUP_TABLE default\n".format(l + 1) + point_data[l]
     with open("{}.vtk".format(outputname), "w") as f:
         f.write(vtk)
 
@@ -112,14 +111,14 @@ def write_vtm(n_wake=0, outputname="agps", inputfile="agps"):
     """convert agps networks to paraview unstructured grid
     each network will become a different vtu file
     to open all vtu files at the same time, open the vtm file with paraview"""
-    data = read_agps(inputfile) # read agps file & specify the number of networks to omit
+    data = read_agps(inputfile)  # read agps file & specify the number of networks to omit
     print("n_wake = ", n_wake)
     # write header of vtm file
     vtm = "<?xml version=\"1.0\"?>\n"
     vtm += "<VTKFile type=\"vtkMultiBlockDataSet\" version=\"1.0\" byte_order=\"LittleEndian\">\n"
     vtm += "  <vtkMultiBlockDataSet>\n"
 
-    for i in range(len(data)-n_wake):
+    for i in range(len(data) - n_wake):
         # add dataset to vtm file
         vtu_dir = "{}_vtu".format(outputname)
         try:
@@ -127,8 +126,8 @@ def write_vtm(n_wake=0, outputname="agps", inputfile="agps"):
         except OSError:
             if not os.path.exists(vtu_dir):
                 raise
-        vtu_path = "{0}/{1}{2}.vtu".format(vtu_dir, outputname, i+1)
-        vtm += "    <DataSet index=\"network{0}\" file=\"{1}\"/>\n".format(i+1, vtu_path)
+        vtu_path = "{0}/{1}{2}.vtu".format(vtu_dir, outputname, i + 1)
+        vtm += "    <DataSet index=\"network{0}\" file=\"{1}\"/>\n".format(i + 1, vtu_path)
         # write header of vtu file
         vtu = "<?xml version=\"1.0\"?>\n"
         vtu += "<VTKFile type=\"UnstructuredGrid\" version=\"1.0\" byte_order=\"LittleEndian\">\n"
@@ -146,15 +145,15 @@ def write_vtm(n_wake=0, outputname="agps", inputfile="agps"):
         # format the agps data
         points = str()  # coordinate of each point (x, y, z)
         cells = str()  # ids of each quadrilateral cell (e.g. (0, n_col, n_col + 1, 1) for first cell)
-        base_square = np.array((0, n_col, n_col+1, 1), dtype=int)
+        base_square = np.array((0, n_col, n_col + 1, 1), dtype=int)
         for j in range(n_row):
             for k in range(n_col):
-                point = net[j,k]
+                point = net[j, k]
                 # add coordinate of a point
                 points += "{0} {1} {2}\n".format(point[1], point[2], point[3])
                 # add ids of a cell
-                if not j == n_row-1 and not k == n_col-1:
-                    square =  base_square + (j * n_col + k)
+                if not j == n_row - 1 and not k == n_col - 1:
+                    square = base_square + (j * n_col + k)
                     square = (str(p) for p in square)
                     cells += " ".join(square) + "\n"
 
@@ -181,7 +180,7 @@ def write_vtm(n_wake=0, outputname="agps", inputfile="agps"):
         vtu += cells
         vtu += "        </DataArray>\n"
         vtu += "        <DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\">\n"
-        vtu += " ".join(str(4*(icell+1)) for icell in range(n_cells)) + "\n"
+        vtu += " ".join(str(4 * (icell + 1)) for icell in range(n_cells)) + "\n"
         vtu += "        </DataArray>\n"
         vtu += "        <DataArray type=\"Int32\" Name=\"types\" format=\"ascii\">\n"
         vtu += " ".join(str(9) for _ in range(n_cells)) + "\n"
@@ -200,15 +199,15 @@ def write_vtm(n_wake=0, outputname="agps", inputfile="agps"):
 
 def write_tec(n_wake=0, outputname="agps", inputfile="agps"):
     """convert agps networks to tecplot finite element quadrilaterals"""
-    data = read_agps(inputfile) # read agps file & specify the number of networks to omit
+    data = read_agps(inputfile)  # read agps file & specify the number of networks to omit
     print("n_wake = ", n_wake)
     # write header
-    n_headers = data[0].shape[2] # number of headers (e.g. 8 for "irow, x, y, z, cp1, cp2, cp3, cp4")
-    n_cp = n_headers - 4 # number of different cps in agps file
+    n_headers = data[0].shape[2]  # number of headers (e.g. 8 for "irow, x, y, z, cp1, cp2, cp3, cp4")
+    n_cp = n_headers - 4  # number of different cps in agps file
     tec = "TITLE = \"AGPS 3D Finite Element Data\"\n"
     tec += "VARIABLES = \"x\", \"y\", \"z\""
     for i in range(n_cp):
-        tec += ", \"cp{}\"".format(i+1)
+        tec += ", \"cp{}\"".format(i + 1)
     tec += "\n"
 
     # write each network as a block
@@ -217,11 +216,11 @@ def write_tec(n_wake=0, outputname="agps", inputfile="agps"):
         net = data[i]
         n_row = int(net.shape[0])
         n_col = int(net.shape[1])
-        print("network {} shape: ".format(i+1), net.shape)
+        print("network {} shape: ".format(i + 1), net.shape)
         n_points = n_row * n_col
-        n_elements = (n_row-1) * (n_col-1)
+        n_elements = (n_row - 1) * (n_col - 1)
         tec += "ZONE T=\"MIXED\", N={}, E={}, DATAPACKING=BLOCK," \
-                  " ZONETYPE=FEQUADRILATERAL\n".format(n_points, n_elements)
+               " ZONETYPE=FEQUADRILATERAL\n".format(n_points, n_elements)
         # write coordinates (x, y, z) and cps (cp1, cp2, cp3, cp4) in each row
         for l in range(1, n_headers):
             element = net[:, :, l]
@@ -236,8 +235,8 @@ def write_tec(n_wake=0, outputname="agps", inputfile="agps"):
         #         quads += " ".join(square) + "\n"
         # same as the above code, but faster evaluation
         quads = "\n".join("\n".join((" ".join((str(p) for p in (base_square + j * n_col + k))))
-                     for k in range(n_col-1))
-                    for j in range(n_row-1))
+                                    for k in range(n_col - 1))
+                          for j in range(n_row - 1))
         tec += quads
     with open("{}.dat".format(outputname), "w") as f:
         f.write(tec)
