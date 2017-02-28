8a760be062739153a25181978f5afe612489b63a
Unknown <s.takanashi0120@gmail.com>
2017-02-28 17:01:05 +0900

    Fix ffmf_converter

--- pyPanair/postprocess/ffmf_converter.py
+++ pyPanair/postprocess/ffmf_converter.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
-__author__ = "stakanashi"
 import pandas as pd
 
 
 def read_ffmf(inpfilepath="ffmf"):
-    ffmf = []
-    columns = ["sol-no", "AoA", "beta", "cl", "cdi", "cy", "fx", "fy", "fz", "mx", "my", "mz", "area"]
+    ffmf = list()
+    columns = ["sol-no", "alpha", "beta", "cl", "cdi", "cy", "fx", "fy", "fz", "mx", "my", "mz", "area"]
     sol_num = 1
     with open(inpfilepath, "r") as f:
-        for i in range(30): # force / torque coefficients are listed in the first 30 rows
+        for i in range(30):  # force/torque coefficients are listed in the first 30 rows
             line = f.readline().split()
-            if line == []:
+            if line:
                 pass
             # read force / torque coefficients
             elif line[0] == str(sol_num):
@@ -23,6 +22,6 @@ def read_ffmf(inpfilepath="ffmf"):
     return ffmf
 
   
-def write_ffmf(outfilepath="ffmf.csv"):
-    ffmf = read_ffmf()
+def write_ffmf(outfilepath="ffmf.csv", inpfilepath="ffmf"):
+    ffmf = read_ffmf(inpfilepath)
     ffmf.to_csv(outfilepath, index=None)
