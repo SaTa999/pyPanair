c61833134381a42bcb8c655b828f4d7f88dbcd9f
Unknown <s.takanashi0120@gmail.com>
2017-03-01 15:31:37 +0900

    Refactor twist optimization

--- examples/twist_optimization/MPQPSO_ADODG3.py
+++ examples/twist_optimization/MPQPSO_ADODG3.py
@@ -17,21 +17,22 @@ import clean_rwms
 from pyPanair.utilities import bspline
 from pyPanair.postprocess import read_ffmf
 
+
 def latin_hypercube_sampling(size, n):
     """standard latin hypercube sampling
     size: number of dimensions
     n: number of samples"""
-    cut = np.linspace(0, 1, n + 1)   
+    cut = np.linspace(0, 1, n + 1)
     u = np.random.rand(n, size)
     a = cut[:n, np.newaxis]
-    b = cut[1:n+1, np.newaxis] 
+    b = cut[1:n + 1, np.newaxis]
     rdpoints = u * (b - a) + a
     # Make the random pairings
-    H = np.zeros_like(rdpoints)
+    h = np.zeros_like(rdpoints)
     for j in range(size):
         order = np.random.permutation(list(range(n)))
-        H[:, j] = rdpoints[order, j]
-    return H
+        h[:, j] = rdpoints[order, j]
+    return h
 
 
 def lhs_swarm(generator, size, n, pmin, pmax):
@@ -40,10 +41,10 @@ def lhs_swarm(generator, size, n, pmin, pmax):
     size: number of dimensions
     n: number of samples
     pmin, pmax: min & max particle position"""
-    H = latin_hypercube_sampling(size, n)
-    H *= (pmax - pmin)
-    H += pmin
-    population = [generator(position=x) for x in H]
+    h = latin_hypercube_sampling(size, n)
+    h *= (pmax - pmin)
+    h += pmin
+    population = [generator(position=x) for x in h]
     return population
 
 
@@ -57,8 +58,8 @@ def generate(creator, size, pmin, pmax, position=None):
         part = creator(np.random.uniform(pmin, pmax, size))
     else:
         part = creator(position)
-    part.pmin=pmin
-    part.pmax=pmax 
+    part.pmin = pmin
+    part.pmax = pmax
     return part
 
 
@@ -66,10 +67,10 @@ def updateParticle(part, best, meanbest, beta):
     """update the particle's position (type1: quantum behaved PSO by Sun 2004)"""
     phi = np.random.random(len(part))
     p = phi * part.best + (1. - phi) * best
-    k = np.random.randint(0,2,len(part)) * 2 - 1 # random array of -1 & 1
+    k = np.random.randint(0, 2, len(part)) * 2 - 1  # random array of -1 & 1
     u = np.random.random(len(part))
-#     part[:] = p + k * beta * np.abs(meanbest - part) * np.log(1./u)
-    part[:] = p + np.einsum("i,...,i,i->i",k,beta,np.abs(meanbest - part),np.log(1./u))
+    #     part[:] = p + k * beta * np.abs(meanbest - part) * np.log(1./u)
+    part[:] = p + np.einsum("i,...,i,i->i", k, beta, np.abs(meanbest - part), np.log(1. / u))
     part[:] = np.clip(part, part.pmin, part.pmax)
     return part
 
@@ -79,11 +80,11 @@ def updateParticle2(part, best, meanbest, beta):
     increases diversity of particle position (lower chance of getting trapped in local optimum)"""
     phi = np.random.random(len(part))
     p = phi * part.best + (1. - phi) * best
-    k = np.random.randint(0,2,len(part)) * 2 - 1 # random array of -1 & 1
+    k = np.random.randint(0, 2, len(part)) * 2 - 1  # random array of -1 & 1
     u = np.random.random(len(part))
     p += np.random.normal(0, 1, len(part)) * np.sqrt(np.abs(meanbest - part.best))
-#     part[:] = p + k * beta * np.abs(meanbest - part) * np.log(1./u)
-    part[:] = p + np.einsum("i,...,i,i->i",k,beta,np.abs(meanbest - part),np.log(1./u))
+    #     part[:] = p + k * beta * np.abs(meanbest - part) * np.log(1./u)
+    part[:] = p + np.einsum("i,...,i,i->i", k, beta, np.abs(meanbest - part), np.log(1. / u))
     if part.pmin or part.pmax:
         part[:] = np.clip(part, part.pmin, part.pmax)
     return part
@@ -92,7 +93,7 @@ def updateParticle2(part, best, meanbest, beta):
 def sort_cp(part):
     col = (part.shape[0] - 1) // 2
     tmp = part[:-1].reshape(2, col)
-    tmp[:] = tmp[:, tmp[0,:].argsort()]
+    tmp[:] = tmp[:, tmp[0, :].argsort()]
     part[:-1] = tmp.ravel()
 
 
@@ -104,8 +105,9 @@ def run_panin(directory):
     else:
         logger.debug(stdout)
 
+
 def run_panair(directory):
-    process = Popen("panair", stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=target_dir)
+    process = Popen("panair", stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=directory)
     (stdout, stderr) = process.communicate(b"a502.in")
     if stderr:
         logger.error(stderr)
@@ -124,11 +126,11 @@ def evaluate(part):
     auxpath = "{}/ADODG_case3.aux".format(target_dir)
     ffmfpath = "{}/ffmf".format(target_dir)
 
-    nan = (float("nan"), )
+    nan = (float("nan"),)
 
     # define twist func and create aux & wgs
     n_cp = (part.shape[0] - 1) // 2
-    cv = np.zeros((2, n_cp+2))
+    cv = np.zeros((2, n_cp + 2))
     cv[:, 1:-1] = part[:-1].reshape(2, n_cp)
     cv[0, -1] = 1
     cv[1, -1] = part[-1]
@@ -204,7 +206,8 @@ def evaluate(part):
     if cl < 37.5:
         return nan
     else:
-        return (cdi, )
+        return cdi,
+
 
 # set logger
 logger = getLogger(__name__)
@@ -218,24 +221,23 @@ logger.setLevel(INFO)
 logger.addHandler(shandler)
 logger.addHandler(fhandler)
 
-
 if __name__ == '__main__':
     logger.info("started program")
     N_PROCS = 10
     NCP = 4  # number of control points
-    NDV = NCP * 2 + 1 # number of design variables
-    NPOP = 100 # number of particles (more than 3 to 4 times the number of design variables)
-    GEN = 200 # number of maximum generations
-    LOWER = np.array((0., ) * NCP + (-10., ) * (NCP + 1))  # lower bound of particle position x1 x2 x3 x4 y1 y2 y3 y4 y5
-    UPPER = np.array((1., ) * NCP + (5., ) * (NCP + 1))  # upper bound of particle position
-    FITNESS_WEIGHTS = (-1.0,) # maximize:(1.0,), minimize:(-1.0,), don't forget the comma!
-    BETA_INIT = 1.0 # initial value of contraction-expansion coefficient
-    BETA_FIN = 0.5 # final value of contraction-expansion coefficient
+    NDV = NCP * 2 + 1  # number of design variables
+    NPOP = 100  # number of particles (more than 3 to 4 times the number of design variables)
+    GEN = 200  # number of maximum generations
+    LOWER = np.array((0.,) * NCP + (-10.,) * (NCP + 1))  # lower bound of particle position x1 x2 x3 x4 y1 y2 y3 y4 y5
+    UPPER = np.array((1.,) * NCP + (5.,) * (NCP + 1))  # upper bound of particle position
+    FITNESS_WEIGHTS = (-1.0,)  # maximize:(1.0,), minimize:(-1.0,), don't forget the comma!
+    BETA_INIT = 1.0  # initial value of contraction-expansion coefficient
+    BETA_FIN = 0.5  # final value of contraction-expansion coefficient
     """TIPS: large beta -> global search, small beta -> local search
              beta_init: 0.8 to 1.2
              beta_fin : less than 0.6
              beta must be below e^gamma=1.781 to guarantee convergence of the particle"""
-    LOG = True # record logbook & graph
+    LOG = True  # record logbook & graph
 
     # register creators
     creator.create("FitnessMax", base.Fitness, weights=FITNESS_WEIGHTS)
@@ -263,13 +265,13 @@ if __name__ == '__main__':
     pop = toolbox.population(n=NPOP)
     best = None
     betas = np.linspace(BETA_INIT, BETA_FIN, GEN)
-    meanbest = np.empty(len(pop[0])) # initialize the meanbest
+    meanbest = np.empty(len(pop[0]))  # initialize the meanbest
     fitness_all = np.empty((NPOP,))
 
     with futures.ProcessPoolExecutor(max_workers=N_PROCS) as executor:
 
         for g, beta in zip(list(range(GEN)), betas):
-            meanbest[:] = 0. # reinitialize the meanbest
+            meanbest[:] = 0.  # reinitialize the meanbest
             INVALID = 0
             # sort the x-coordinates of the contril points
             for part in pop:
@@ -283,7 +285,7 @@ if __name__ == '__main__':
             # MP region end
 
             for i, part in enumerate(pop):
-                part.fitness.values = (fitness_all[i], )
+                part.fitness.values = (fitness_all[i],)
                 if part.fitness.values[0] is float("nan"):
                     INVALID += 1
                 # reinitialize the particle if its initial fitness value is nan
@@ -307,9 +309,10 @@ if __name__ == '__main__':
                 toolbox.update(part, best, meanbest, beta)
 
             # Gather all the fitnesses in one list and print the stats
-            logbook.record(gen=g, evals=len(pop)*(g+1), gbest=best.fitness.values[0], invalid=INVALID,  **stats.compile(pop))
+            logbook.record(gen=g, evals=len(pop) * (g + 1), gbest=best.fitness.values[0],
+                           invalid=INVALID, **stats.compile(pop))
             if LOG:
-                print((logbook.stream))
+                print(logbook.stream)
             logger.info("end of generation {}".format(g))
 
     print("end of global optimization")
@@ -332,7 +335,6 @@ if __name__ == '__main__':
         fit_mins = logbook.select("min")
         fit_avgs = logbook.select("mean")
         fit_stds = logbook.select("std")
-
 
         fig, ax1 = plt.subplots()
         line1 = ax1.plot(gen, fit_mins, "b-", label="Minimum Fitness")
